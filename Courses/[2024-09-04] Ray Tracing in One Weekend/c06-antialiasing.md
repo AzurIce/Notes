d6d5190783edd1afec7d6f2b3aab6d4c035179a7
AzurIce <973562770@qq.com>
2024-09-07 00:39:47 +0800

    feat(Ray Tracing in One Weekend): antialiasing

---
+++ Courses/[2024-09-04] Ray Tracing in One Weekend/c06-antialiasing.md
@@ -0,0 +1,77 @@
+# c06-antialiasing
+
+<img src="./assets/image-20240907001025528.png" alt="image-20240907001025528" style="zoom:40%;" /><img src="./assets/image-20240907003819882.png" alt="image-20240907003819882" style="zoom:40%;" />
+
+---
+
+<img src="./assets/image-20240907001025528.png" alt="image-20240907001025528" style="zoom:50%;" />
+
+放大观察生成的图像，可以看到边缘处有锯齿的形状，这就是走样。
+
+为何会有这种现象出现？现实中的相机的成像过程与光线追踪的过程几乎完全符合，问题出在哪里？
+
+在我的解释方式中（更详细的见 A Pixel Is Not A Little Square[^1]），原因是，现实的相机传感器上的某一个像素所接受到的光照，并不是单一来源的。一个像素并不是一个点，而是有面积的。而目前实现的光线追踪中对于每一个像素只采样了一个像素。
+
+那么很自然可以得到一种解决方案 —— 对于一个像素，在其范围内进行多次随机采样并混合得到的颜色。
+
+## 一、反走样
+
+为 `Camera` 添加一个 `samples_per_pixel` 属性，然后修改 `render_to_ppm`：
+
+```rust
+impl Camera {
+    pub fn render_to_ppm(
+        &self,
+        world: &Vec<Box<dyn Hittable>>,
+        output_width: u32,
+        path: impl AsRef<Path>,
+    ) {
+        // ...
+      
+        for j in 0..output_height {
+            for i in 0..output_width {
+                let mut color = Vec3::ZERO;
+
+                let pixel_center =
+                    pixel00_loc + (i as f32 * pixel_delta_u) + (j as f32 * pixel_delta_v);
+                for _ in 0..self.samples_per_pixel {
+                    let rand_offset = Vec3::new(
+                        (random::<f32>() - 0.5) * pixel_delta_u.x,
+                        (random::<f32>() - 0.5) * pixel_delta_v.y,
+                        0.0,
+                    );
+
+                    let ray = Ray::new(
+                        self.camera_center,
+                        pixel_center + rand_offset - self.camera_center,
+                    );
+
+                    color += ray_color(&ray, world);
+                }
+                color /= self.samples_per_pixel as f32;
+
+                // ...
+                pb.inc(1);
+            }
+        }
+        pb.finish();
+        multi.remove(&pb);
+    }
+}
+```
+
+对比效果如下：
+
+<img src="./assets/image-20240907001025528.png" alt="image-20240907001025528" style="zoom:40%;" /><img src="./assets/image-20240907003348120.png" alt="image-20240907003348120" style="zoom:40%;" />
+
+还可以进一步增加 `samples_per_pixel` 至 100：
+
+<img src="./assets/image-20240907001025528.png" alt="image-20240907001025528" style="zoom:40%;" /><img src="./assets/image-20240907003819882.png" alt="image-20240907003819882" style="zoom:40%;" />
+
+输出图片：
+
+![image_c06](./assets/image_c06.png)
+
+## 参考
+
+[^1]: [(PDF) A Pixel Is Not A Little Square, A Pixel Is Not A Little Square, A Pixel Is Not A Little Square](https://www.researchgate.net/publication/244986797_A_Pixel_Is_Not_A_Little_Square_A_Pixel_Is_Not_A_Little_Square_A_Pixel_Is_Not_A_Little_Square)
---
+++ Courses/[2024-09-04] Ray Tracing in One Weekend/raytracing/workspace/c06-antialiasing/src/camera.rs
@@ -0,0 +1,149 @@
+use std::{
+    fs::File,
+    io::{BufWriter, Write},
+    path::Path,
+};
+
+use crate::{log::logger, Hittable, Ray};
+use ::log::info;
+use glam::Vec3;
+use indicatif::ProgressBar;
+use rand::random;
+
+pub fn ray_color(ray: &Ray, world: &Vec<Box<dyn Hittable>>) -> Vec3 {
+    if let Some(record) = world.hit(ray, 0.0..f32::INFINITY) {
+        let n = record.normal.normalize();
+        return 0.5 * (Vec3::new(n.x, n.y, n.z) + 1.0);
+    }
+
+    let unit_direction = ray.direction.normalize();
+    let a = 0.5 * (unit_direction.y + 1.0); // 从 [-1, 1] 映射到 [0, 1]
+    (1.0 - a) * Vec3::new(1.0, 1.0, 1.0) + a * Vec3::new(0.5, 0.7, 1.0)
+}
+
+pub struct Camera {
+    focal_length: f32,
+    aspect_ratio: f32,
+    viewport_height: f32,
+    viewport_width: f32,
+    camera_center: Vec3,
+    samples_per_pixel: u32,
+}
+
+impl Default for Camera {
+    fn default() -> Self {
+        let aspect_ratio = 16.0 / 9.0;
+        let viewport_height = 2.0;
+        let viewport_width = viewport_height * aspect_ratio;
+
+        Self {
+            focal_length: 1.0,
+            aspect_ratio,
+            viewport_height,
+            viewport_width,
+            camera_center: Vec3::ZERO,
+            samples_per_pixel: 100,
+        }
+    }
+}
+
+impl Camera {
+    pub fn new(aspect_ratio: f32) -> Self {
+        let viewport_height = 2.0;
+        let viewport_width = viewport_height * aspect_ratio;
+
+        Camera {
+            aspect_ratio,
+            viewport_height,
+            viewport_width,
+            ..Default::default()
+        }
+    }
+
+    pub fn camera_center(mut self, camera_center: Vec3) -> Self {
+        self.camera_center = camera_center;
+        self
+    }
+
+    pub fn focal_length(mut self, focal_length: f32) -> Self {
+        self.focal_length = focal_length;
+        self
+    }
+
+    pub fn samples_per_pixel(mut self, samples_per_pixel: u32) -> Self {
+        self.samples_per_pixel = samples_per_pixel;
+        self
+    }
+}
+
+impl Camera {
+    pub fn render_to_ppm(
+        &self,
+        world: &Vec<Box<dyn Hittable>>,
+        output_width: u32,
+        path: impl AsRef<Path>,
+    ) {
+        let file = File::create(path).unwrap();
+        let mut writer = BufWriter::new(file);
+
+        // Camera
+        let viewport_u = Vec3::new(self.viewport_width, 0.0, 0.0);
+        let viewport_v = Vec3::new(0.0, -self.viewport_height, 0.0);
+
+        let output_height = (output_width as f32 / self.aspect_ratio) as u32;
+
+        let pixel_delta_u = viewport_u / output_width as f32;
+        let pixel_delta_v = viewport_v / output_height as f32;
+
+        let viewport_upper_left = self.camera_center
+            - Vec3::new(0.0, 0.0, self.focal_length)
+            - viewport_u / 2.0
+            - viewport_v / 2.0;
+        let pixel00_loc = viewport_upper_left + 0.5 * (pixel_delta_u + pixel_delta_v);
+
+        info!("generating image...");
+        writer
+            .write_all(format!("P3\n{} {}\n255", output_width, output_height).as_bytes())
+            .unwrap();
+
+        let multi = logger().multi();
+        let pb = multi.add(ProgressBar::new(output_height as u64));
+        for j in 0..output_height {
+            for i in 0..output_width {
+                let mut color = Vec3::ZERO;
+
+                let pixel_center =
+                    pixel00_loc + (i as f32 * pixel_delta_u) + (j as f32 * pixel_delta_v);
+                for _ in 0..self.samples_per_pixel {
+                    let rand_offset = Vec3::new(
+                        (random::<f32>() - 0.5) * pixel_delta_u.x,
+                        (random::<f32>() - 0.5) * pixel_delta_v.y,
+                        0.0,
+                    );
+
+                    let ray = Ray::new(
+                        self.camera_center,
+                        pixel_center + rand_offset - self.camera_center,
+                    );
+
+                    color += ray_color(&ray, world);
+                }
+                color /= self.samples_per_pixel as f32;
+
+                let color_u8 = (255.999 * color).clamp(Vec3::ZERO, Vec3::splat(255.0));
+                writer
+                    .write_all(
+                        format!(
+                            "\n{} {} {}",
+                            color_u8.x as u32, color_u8.y as u32, color_u8.z as u32
+                        )
+                        .as_bytes(),
+                    )
+                    .unwrap();
+                pb.inc(1);
+            }
+        }
+        pb.finish();
+        multi.remove(&pb);
+    }
+}
