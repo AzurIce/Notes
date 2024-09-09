2b0d8bc2c83c7990181f28c6508a7926418056b0
AzurIce <973562770@qq.com>
2024-09-09 16:32:09 +0800

    prepare for Ray Tracing The Next Week

---
+++ Courses/[2024-09-08] Ray Tracing The Next Week/README.md
@@ -0,0 +1,5 @@
+# Ray Tracing: The Next Week
+
+>  [!NOTE]
+>
+> 本书内容为 [Ray Tracing: The Next Week]([Ray Tracing: The Next Week](https://raytracing.github.io/books/RayTracingTheNextWeek.html)) 的 Rust 实现
---
+++ Courses/[2024-09-08] Ray Tracing The Next Week/raytracing/flake.nix
@@ -0,0 +1,54 @@
+{
+  description = "ice";
+
+  nixConfig = {
+    extra-substituters = [
+      "https://mirrors.ustc.edu.cn/nix-channels/store"
+    ];
+    trusted-substituters = [
+      "https://mirrors.ustc.edu.cn/nix-channels/store"
+    ];
+  };
+
+
+  inputs = {
+    nixpkgs.url      = "github:NixOS/nixpkgs/nixos-unstable";
+    rust-overlay.url = "github:oxalica/rust-overlay";
+    flake-utils.url  = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, rust-overlay, flake-utils, ... }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        overlays = [ (import rust-overlay) ];
+        pkgs = import nixpkgs {
+          inherit system overlays;
+        };
+      in
+      {
+        devShells.default = pkgs.mkShell {
+          buildInputs = with pkgs; [
+            # clang
+            # llvmPackages_16.bintools
+            # libusb1
+            # openssl
+            # pkg-config
+            (rust-bin.nightly.latest.default.override {
+              extensions = [ "rust-src" ];
+            })
+          ]
+          ++
+          (pkgs.lib.optionals pkgs.stdenv.isDarwin (with pkgs; [
+            # libiconv-darwin
+          ]) ++ (with pkgs.darwin.apple_sdk.frameworks; [
+            # SystemConfiguration
+          #   IOKit
+            # Security
+            # CoreFoundation
+          #   AppKit
+          ]))
+          ;
+        };
+      }
+    );
+}
